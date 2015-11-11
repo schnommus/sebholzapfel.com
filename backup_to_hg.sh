9d8d90089f45b81a9f39b04b71e6976005bf9296
schnommus <schnommus@localhost>
2015-11-11 08:51:39 -0500

    Backing up website...

--- backup_to_hg.sh
+++ backup_to_hg.sh
@@ -1,4 +1,8 @@
 cd .
+sudo rm -r -f www_bk
+sudo rm -r -f current_nginx_config
+cp /etc/nginx/sites-enabled/ghost current_nginx_config
+cp /home/schnommus/www www_bk
 sudo hg add .
 sudo hg commit -u schnommus -m "Backing up website..."
 sudo hg push https://schnommus@bitbucket.org/schnommus/sebholzapfel.com
